7b36c547269d0f5983034ad4515949adf1e1d7e4
josue aceves <josueaceves.ja@gmail.com>
2015-11-17 16:21:35 -0600

    add Users new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new' => 'users#new'
+
+  post 'users/create' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
