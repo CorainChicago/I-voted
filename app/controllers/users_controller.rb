d0235a700c5a0e67b0f5674b79fb6b6cca46c120
CorainChicago <chaysmagan@gmail.com>
2015-11-18 17:20:29 -0600

    add error message to user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.valid?
+      @user.save
       redirect_to "/"
     else
+      @errors = @user.errors.full_messages
       render "new"
     end
   end
