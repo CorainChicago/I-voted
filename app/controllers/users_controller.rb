121bdf1f6701f1c8ee1d6de1bd37f09c2a190aad
CorainChicago <chaysmagan@gmail.com>
2015-11-18 16:56:34 -0600

    made params private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
     end
   end
 
+  private
   def user_params
-    params.require(:user).permit(:email, :password, :address)
+    params.require(:user).permit(:email, :street_address, :city, :state, :zip, :password)
   end
 end
