86e7ba4f86ae86775dfdedca3997c01462e31b46
CorainChicago <chaysmagan@gmail.com>
2015-11-18 16:20:25 -0600

    add columns to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117221158) do
+ActiveRecord::Schema.define(version: 20151118220253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,12 @@ ActiveRecord::Schema.define(version: 20151117221158) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.string   "address"
+    t.string   "street_address"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "city"
+    t.string   "state"
+    t.integer  "zip"
   end
 
 end
