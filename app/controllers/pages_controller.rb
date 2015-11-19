33b52a07f5909bb3893e1b3d9ee80d3f3714ac55
CorainChicago <chaysmagan@gmail.com>
2015-11-19 10:10:10 -0600

    add user controller tests and new seed data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
 
   def index
   end
+
+  def show
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118220253) do
+ActiveRecord::Schema.define(version: 20151119012620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "candidates", force: :cascade do |t|
+    t.string   "name"
+    t.string   "office"
+    t.string   "party_affiliation"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "electioninfos", force: :cascade do |t|
     t.string   "title",      null: false
