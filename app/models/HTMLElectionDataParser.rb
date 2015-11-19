88f3def2c44da0669c8031975464864b9cc477e7
LucyBoone <lucy.a.boone@gmail.com>
2015-11-19 14:14:02 -0600

    election data parsing method, should eventually dry this & voter data parser

---
+++ app/models/HTMLElectionDataParser.rb
@@ -0,0 +1,36 @@
+class HTMLElectionDataParser
+  require 'open-uri'
+  require 'nokogiri'
+  require 'json'
+
+  def parse
+    @doc = Nokogiri::HTML(open("https://www.usvotefoundation.org/vote/state-elections/state-election-dates-deadlines.htm"))
+
+    @table = @doc.search("table.election-dates")
+    @rows = @table.search("tr")
+    @data = []
+    counter = 1
+    169.times do
+      state = Hash.new
+      state["Election Title"] = @rows[counter].search("td.election-title").text.strip
+      state["Election DAte"] = @rows[counter].search("td:nth-child(2)").text.strip
+      state["Voter Registration"] = @rows[counter].search("td:nth-child(3)").text.strip
+      state["Absentee Ballot Request"] = @rows[counter].search("td:nth-child(4)").text.strip
+      state["Absentee Ballot Return"] = @rows[counter].search("td:nth-child(5)").text.strip
+      state["Early In-Person Voting"] = @rows[counter].search("td:nth-child(6)").text.strip
+      @data << state
+      counter += 1
+    end
+  end
+
+  def save_data
+    File.open("public/election_data.json","w") do |f|
+      f.write(@data.to_json)
+    end
+  end
+
+end
+
+p = HTMLElectionDataParser.new
+p.parse
+p.save_data
