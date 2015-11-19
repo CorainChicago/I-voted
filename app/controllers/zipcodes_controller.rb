93657b967ad6f36260a545540258481dc4837680
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-19 17:11:48 -0600

    add dotenv gem to allow api key to be accessed

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -1,6 +1,14 @@
+require 'net/http'
+require 'json'
+
 class ZipcodesController < ApplicationController
   def show
     @zipcode = session[:zip]
+    @polling_place = get_polling_place(@zipcode)
   end
-
+  def get_polling_place(zipcode)
+    uri = URI.parse('https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyDkn2kz-xb9COWfsqSVkSePa2G7yHbXnxY')
+    @response = JSON.parse(Net::HTTP.get(uri))
+    puts @response
+  end
 end
