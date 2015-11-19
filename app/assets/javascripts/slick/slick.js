46650f1d3270bc26e825f6ab095932dfb3ce31fc
LucyBoone <lucy.a.boone@gmail.com>
2015-11-19 14:52:26 -0600

    additional slick changes, fixed a merge conflict

--- app/assets/javascripts/slick/slick.js
+++ app/assets/javascripts/slick/slick.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
       $('.image-carousel').slick({
-        // dots: true,
-        // infinite: true,
-        // speed: 100,
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
         centerMode: true,
+        centerPadding: '500px',
         // variableWidth: true
         // adaptiveHeight: true
       });
