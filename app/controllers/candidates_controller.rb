9a07c0ae6130fdf0575aa2749ebbed73e5729e3e
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-19 16:30:59 -0600

    Add check to prevent candidates from being added to a single zipcode multiple times

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,7 +1,9 @@
 class CandidatesController < ApplicationController
   def create
     post_params.each do |candidate|
-      Candidate.create(name: candidate[1][:name], office: candidate[1][:office], party_affiliation: candidate[1][:party], zip_id: session[:zip])
+      if (Candidate.where(zip_id: session[:zip]).where(name: candidate[1][:name]).count == 0)
+          Candidate.create(name: candidate[1][:name], office: candidate[1][:office], party_affiliation: candidate[1][:party], zip_id: session[:zip])
+      end
     end
     render :nothing => true, :status => :ok
   end
