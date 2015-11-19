56356bd8c6c8142d410fede22d6ca49970d5da4e
josue aceves <josueaceves.ja@gmail.com>
2015-11-19 13:55:17 -0600

    add test to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "User" do
+  describe "validations" do
+    it "is not valid without email " do
+      user = User.create
+      user.valid?
+      expect(user.errors).to_not be_empty
+  end
+
+    it "is not valid without street address" do
+      user = User.create(email: "myemail@gmail.com")
+      user.valid?
+      expect(user.errors).to_not be_empty
+    end
+
+    it "is not valid without city" do
+      user = User.create(email: "myemail@gmail.com", street_address: " 351 W Hubbard" )
+      user.valid?
+      expect(user.errors).to_not be_empty
+    end
+
+    it "is not valid without state" do
+      user = User.create(email: "myemail@gmail.com", street_address: " 351 W Hubbard", city: "SM" )
+      user.valid?
+      expect(user.errors).to_not be_empty
+    end
+    it "is not valid without password" do
+      user = User.create(email: "myemail@gmail.com", street_address: " 351 W Hubbard", city: "SM", state: "CA" )
+      user.valid?
+      expect(user.errors).to_not be_empty
+    end
+
+    it "should be valid" do
+      user = User.create(email: "myemail@gmail.com", street_address: " 351 W Hubbard", city: "SM", state: "CA", password: "password")
+      user.valid?
+      expect(user.errors).to be_empty
+    end
+  end
 end
