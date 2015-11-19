1ff0666dc4eb6e3d2011953d5c53465e869bff56
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-19 10:53:50 -0600

    Add candidates controller and route

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,5 @@
+class CandidatesController < ApplicationController
+  def create
+    redirect_to ''
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'sessions/logout' => 'sessions#logout'
 
+  post 'candidates/create' => 'candidates#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
