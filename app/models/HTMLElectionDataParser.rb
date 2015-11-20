da775b9a37d285e84e395d3e4a1fadb7b587518f
LucyBoone <lucy.a.boone@gmail.com>
2015-11-19 20:23:40 -0600

    play with method to see if that fixes seed issue

--- app/models/HTMLElectionDataParser.rb
+++ app/models/HTMLElectionDataParser.rb
@@ -12,19 +12,19 @@ class HTMLElectionDataParser
     counter = 1
     169.times do
       state = Hash.new
-      state["Election Title"] = @rows[counter].search("td.election-title").text.strip
-      state["Election Date"] = @rows[counter].search("td:nth-child(2)").text.strip
-      state["Voter Registration"] = @rows[counter].search("td:nth-child(3)").text.strip
-      state["Absentee Ballot Request"] = @rows[counter].search("td:nth-child(4)").text.strip
-      state["Absentee Ballot Return"] = @rows[counter].search("td:nth-child(5)").text.strip
-      state["Early In-Person Voting"] = @rows[counter].search("td:nth-child(6)").text.strip
+      state["Election Title"] = @rows[counter].search("td.election-title").text
+      state["Election Date"] = @rows[counter].search("td:nth-child(2)").text
+      state["Voter Registration"] = @rows[counter].search("td:nth-child(3)").text
+      state["Absentee Ballot Request"] = @rows[counter].search("td:nth-child(4)").text
+      state["Absentee Ballot Return"] = @rows[counter].search("td:nth-child(5)").text
+      state["Early In-Person Voting"] = @rows[counter].search("td:nth-child(6)").text
       @data << state
       counter += 1
     end
   end
 
   def save_data
-    File.open("election_data.json","w") do |f|
+    File.open("public/election_data.json","w") do |f|
       f.write(@data.to_json)
     end
   end
