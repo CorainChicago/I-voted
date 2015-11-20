5735c17f41ad7f9ed12c285f8dec2c30aa67bdd0
CorainChicago <chaysmagan@gmail.com>
2015-11-20 10:12:33 -0600

    fix bug in show page by replacing session[:zip] with params[:zip]

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -4,7 +4,7 @@ require 'json'
 class ZipcodesController < ApplicationController
 
   def show
-    @zipcode = session[:zip]
+    @zipcode = params["zip"]
     @zip = Zipcode.find_by(zip: @zipcode)
     @voter_registration_data = StateVotingInformation.find_by(name: @zip.state_name)
   end
