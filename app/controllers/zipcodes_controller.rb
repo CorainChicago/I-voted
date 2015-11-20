0bb2bd60c0b37fb90d3acf1c6bdd640ded5cf090
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-19 20:58:57 -0600

    Move poling place locator to model

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -6,10 +6,4 @@ class ZipcodesController < ApplicationController
     @zipcode = session[:zip]
     @polling_place = get_polling_place(address)
   end
-  def self.get_polling_place(address)
-    uri = URI.parse("https://www.googleapis.com/civicinfo/v2/voterinfo?key=#{ENV['API_KEY']}&address=#{address.gsub(' ', '%20')}&electionId=2000")
-    response = JSON.parse(Net::HTTP.get(uri))
-    return response['pollingLocations'][0] if response.length > 1
-    {'locationName' => 'Sorry, we can\'t find a polling place for your location at this time. Please try a different address or check back closer to election day.'}
-  end
 end
