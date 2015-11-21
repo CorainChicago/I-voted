feefc2d1491d0b0092ac34b9af9f7ceca63be7a5
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-20 20:45:19 -0600

    Add congressional district

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,10 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @zip = session[:zip]
     @candidates = Candidate.where(zip: @zip).where("name != ?", "Barack Obama II")
+    @district = Zipcode.get_district(("#{@user.street_address} #{@user.city}, #{@user.state} #{@zip}").gsub(' ', "%20")).gsub('s\'s', 's\'')
     polling_place = Zipcode.get_polling_place(("#{@user.street_address} #{@user.city}, #{@user.state} #{@zip}").gsub(' ', "%20"))['address']
     @polling_place = polling_place['locationName'] + ', ' + polling_place['line1'] + '. ' +  polling_place['city'] + ', ' + polling_place['state'] + " " + polling_place['zip']
+    puts @district
   end
 
   private
