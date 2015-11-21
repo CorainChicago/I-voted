ebe2fd8060c73035a095008d274bc4218bb03368
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-20 20:34:22 -0600

    Add polling place

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,8 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @zip = session[:zip]
     @candidates = Candidate.where(zip: @zip).where("name != ?", "Barack Obama II")
+    polling_place = Zipcode.get_polling_place(("#{@user.street_address} #{@user.city}, #{@user.state} #{@zip}").gsub(' ', "%20"))['address']
+    @polling_place = polling_place['locationName'] + ', ' + polling_place['line1'] + '. ' +  polling_place['city'] + ', ' + polling_place['state'] + " " + polling_place['zip']
   end
 
   private
