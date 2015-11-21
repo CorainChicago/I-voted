881f41b133aba1d5803e2b5a8a544d7dabce960b
LucyBoone <lucy.a.boone@gmail.com>
2015-11-21 16:49:33 -0600

    test for new user view

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,19 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "users/new.html.erb", type: :view do
-  scenario "user creates new account" do
+feature "User creates new account" do
+  scenario "user views page" do
     visit 'users/new'
     expect(page).to have_content("Sign Up")
-    expect(page).has_field("Email:" :type => :email )
+    expect(page).to have_content("Email:")
     expect(page).to have_content("Password:")
     expect(page).to have_content("Street address:")
     expect(page).to have_content("City:")
     expect(page).to have_content("State:")
     expect(page).to have_content("Zip:")
   end
-
-  # scenario "registration page is distinct from login" do
-  #   visit 'users/new'
-  #
-  # end
 end
