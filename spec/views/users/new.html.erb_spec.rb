f7d147dc586f7728e6e1aa38073416c957e9f4f9
LucyBoone <lucy.a.boone@gmail.com>
2015-11-21 15:51:24 -0600

    initial commit for new user tests

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "users/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  scenario "user creates new account" do
+    visit 'users/new'
+    expect(page).to have_content("Sign Up")
+    expect(page).has_field("Email:" :type => :email )
+    expect(page).to have_content("Password:")
+    expect(page).to have_content("Street address:")
+    expect(page).to have_content("City:")
+    expect(page).to have_content("State:")
+    expect(page).to have_content("Zip:")
+  end
+
+  # scenario "registration page is distinct from login" do
+  #   visit 'users/new'
+  #
+  # end
 end
