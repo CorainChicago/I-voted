e13889d2615500a1f638ce78d6c15f177b9be80e
josue aceves <josueaceves.ja@gmail.com>
2015-11-21 19:59:26 -0600

    add some styling to user show page

--- app/assets/javascripts/toggle_candidate_list.js
+++ app/assets/javascripts/toggle_candidate_list.js
@@ -1,10 +1,6 @@
 $(document).ready(function(){
-
-
-
-  $(".candite-name-list").on("click", function(){
+  $(".candidate-name-list").on("click", function(){
     var div = $(this).attr("id")
     $("a[id="+ div+"]").siblings("div").toggle();
-
   })
 })
