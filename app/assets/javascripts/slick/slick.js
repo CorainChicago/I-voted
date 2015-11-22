81820064c94e162f4471cdeb7d820e562b33c77b
LucyBoone <lucy.a.boone@gmail.com>
2015-11-22 14:10:23 -0600

    add 1024 breakpoint back in

--- app/assets/javascripts/slick/slick.js
+++ app/assets/javascripts/slick/slick.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
               centerMode: false,
             }
           },
-          // {
-          //   breakpoint: 1024,
-          //   settings: {
-          //     slidesToShow: 3,
-          //     slidesToScroll: 1,
-          //     centerMode: false,
-          //   }
-          // },
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              centerMode: false,
+            }
+          },
           {
             breakpoint: 640,
             settings: {
