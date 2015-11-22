c587e1c346cbab3fc68995c057533ece7adc43b6
LucyBoone <lucy.a.boone@gmail.com>
2015-11-21 21:02:32 -0600

    add improperly formatted email test

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
+
 feature "User creates new account" do
-  scenario "user views page" do
+  background do
+    User.make(:email => 'example@i-voted.com', :password => 'ivoted1', :street_address => '1600 Pennsylvania Ave', :city => 'Washington', :state => 'DC', :zip => '20500')
+  end
+
+  scenario "page loads correctly" do
     visit 'users/new'
     expect(page).to have_content("Sign Up")
     expect(page).to have_content("Email:")
@@ -11,4 +16,19 @@ feature "User creates new account" do
     expect(page).to have_content("State:")
     expect(page).to have_content("Zip:")
   end
+  scenario "user registers with improperly formatted email" do
+    visit 'users/new'
+    within("#user") do
+      fill_in "Email:" :with => 'example@i-voted'
+      fill_in "Password:" :with => 'ivoted1'
+      fill_in "Street address:" :with => '1600 Pennsylvania Ave'
+      fill_in "City:" :with => 'Washington'
+      fill_in "State:" :with => 'DC'
+      fill_in "Zip:" :with => '20500'
+    end
+    click_button 'Register'
+    expect(page).to have_content("Email is invalid")
+    end
+  end
+
 end
