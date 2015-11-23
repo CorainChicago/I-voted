361c246ab161f4821a2437816702d73975b1c0c0
CorainChicago <chaysmagan@gmail.com>
2015-11-23 09:57:01 -0600

    fix route to logout button and method

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  delete '/logout'  => 'sessions#destroy'
 
   # get 'users/new' => 'users#new'
 
