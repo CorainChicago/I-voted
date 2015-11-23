6df05f84d4b1db8d3d9fcc8d53b91a405d75e716
CorainChicago <chaysmagan@gmail.com>
2015-11-23 16:31:28 -0600

    basic map on user show page: need to style and imput correct coordinates

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,11 +1,22 @@
-// var map = new GMap2(document.getElementById("map_canvas"));
-var geocoder = new GClientGeocoder();
+// function initialize() {
+//   var mapProp = {
+//   center:new google.maps.LatLng(51.508742, -0.120850),
+//   zoom: 7,
+//   mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+// }
 
-var address = "1505 W. Wilson, Chicago Illinois";
-geocoder.getLatLng(address, function(point) {
-         var latitude = point.y;
-         var longitude = point.x; 
-         console.log(latitude); 
-
-         // do something with the lat lng
-    });
+// google.maps.event.addDomListener(window, 'load', initialize);
+$(document).ready(function(){
+  function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+      center: new google.maps.LatLng(44.5403, -78.5463),
+      zoom: 11,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
