3eb2758648584d6de98c363bc69b7db03e7ffc09
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-23 17:28:35 -0600

    Fixed the creation of candidates

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,7 +2,7 @@ class CandidatesController < ApplicationController
   def create
     post_params.each do |candidate|
       if (Candidate.where(zip: zip_params[:zip]).where(name: candidate[1][:name]).count == 0)
-          Candidate.create(name: candidate[1][:name], office: candidate[1][:office], party_affiliation: candidate[1][:party], zip: session[:zip])
+          Candidate.create(name: candidate[1][:name], office: candidate[1][:office], party_affiliation: candidate[1][:party], zip: zip_params[:zip])
       end
     end
     render :nothing => true, :status => :ok
