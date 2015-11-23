c198daba401847bed853d8afb39f9fc76e454979
LucyBoone <lucy.a.boone@gmail.com>
2015-11-23 11:06:19 -0600

    still fiddling with slick

--- app/assets/javascripts/slick/slick.js
+++ app/assets/javascripts/slick/slick.js
@@ -1,25 +1,28 @@
 $(document).ready(function(){
       $('.image-carousel').slick({
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         centerMode: true,
-        centerPadding: '95px',
+        centerPadding: '525px',
+        autoplay: false,
         adaptiveHeight: true,
         responsive: [
           {
             breakpoint: 1440,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              centerMode: false,
+              slidesToShow: 1,
+              centerMode: true,
+              adaptiveHeight: true,
+
             }
           },
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
+              slidesToShow: 1,
               slidesToScroll: 1,
-              centerMode: false,
+              fade: true,
+              cssEase: 'linear'
             }
           },
           {
