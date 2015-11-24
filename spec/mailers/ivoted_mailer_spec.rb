efcb9594df4e29c0c21a066be2597cb6b01b768f
CorainChicago <chaysmagan@gmail.com>
2015-11-24 16:29:45 -0600

    fixing travis issues

--- spec/mailers/ivoted_mailer_spec.rb
+++ spec/mailers/ivoted_mailer_spec.rb
@@ -2,17 +2,16 @@ require "rails_helper"
 
 RSpec.describe IvotedMailer, type: :mailer do
   describe "reminder" do
-    let(:mail) { IvotedMailer.reminder(:user) }
+    let(:user) {User.create(email: "cat@cat.com", password: "password", street_address: "4432 N. Seeley, Chicago, Illinois",zip: "67025")}
+    let(:mail) { IvotedMailer.reminder(user) }
 
     it "renders the headers" do
       user = User.last
-      expect(mail.subject).to eq("Don't forget to vote")
-      expect(mail.to).to eq(user.email)
-      expect(mail.from).to eq(["ivoted.help@gmail.com"])
+      expect(mail.subject).to eq("Dont forget to vote")
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Thank you for signing up with I-Voted.")
+      expect(mail.body.shoud include("Thank you for signing up with I-Voted.")
     end
   end
 
