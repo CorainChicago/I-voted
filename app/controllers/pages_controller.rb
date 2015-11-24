cfa17ab7a4a2936b4156732122f7a02249e704b2
josue aceves <josueaceves.ja@gmail.com>
2015-11-23 18:51:49 -0600

    move email reminder method to application controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,20 +9,4 @@ class PagesController < ApplicationController
   def show
   end
 
-
-
-
-  def send_reminders_email
-    date = Date.parse("%#{DateTime.now.day} %#{DateTime.now.mon}")
-    if date.mon == 11 && date.day == 23
-      User.all.each do |user|
-        if  !user.reminder_emails.last.nil? && user.reminder_emails.last.subject != "elections coming up"
-          IvotedMailer.reminder(user).deliver
-          ReminderEmail.create(user_id: user.id, subject: "elections coming up")
-        end
-      end
-    end
-  end
-
-
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,7 @@ class ApplicationMailer < ActionMailer::Base
   include Resque::Mailer
   default from: "ivoted@reminder.com"
   layout 'mailer'
+
+
+
 end
