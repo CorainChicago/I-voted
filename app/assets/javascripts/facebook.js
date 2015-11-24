de8630c73b5ba38cdba4899b2537c1e6395a7d94
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-24 08:42:54 -0600

    Move script out of html and into .js file

---
+++ app/assets/javascripts/facebook.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  var getKey = $.ajax({
+    url: '/fb_key',
+    method: 'get'
+  });
+  getKey.done(function(response){
+    var key = response;
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : key,
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : key,
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "//connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  });
+});
