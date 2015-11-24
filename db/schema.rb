94a9d903ada6edeb512c600e673188a2b1e3ffcd
KyleDolezal <Kyle.D.Dolezal@gmail.com>
2015-11-23 18:52:15 -0600

    Update Change user zip to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121213030) do
+ActiveRecord::Schema.define(version: 20151124004449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 20151121213030) do
     t.datetime "updated_at",      null: false
     t.string   "city"
     t.string   "state"
-    t.integer  "zip"
+    t.string   "zip"
   end
 
   create_table "zipcodes", force: :cascade do |t|
