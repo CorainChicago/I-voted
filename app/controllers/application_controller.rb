e03ea2ddbd0cc42b961a5117b36d070fc5029958
Kyle Dolezal <Kyle.D.Dolezal@gmail.com>
2016-01-13 01:26:48 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < ActionController::Base
 
 
   def send_reminders_email
-    date = Date.parse("#{DateTime.now.day}/#{DateTime.now.mon}")
+    date = Date.parse("#{DateTime.now.day} / #{DateTime.now.mon}")
     if date.mon == 9 && date.day == 1
       User.all.each do |user|
         if user.subscribe == true && !user.reminder_emails.last.nil? && user.reminder_emails.last.subject != "elections coming up"
