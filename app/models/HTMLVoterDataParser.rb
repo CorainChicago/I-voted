e6c449f93e2b418a5bd91a76d92893a16ef562d3
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2016-12-04 19:21:27 +0800

    Update state voting information parsing and views to reflect changes in US Vote Foundation data. The scraper now picks up the latest data format. Put the parser into a rake task so it can be updated automatically.

--- app/models/HTMLVoterDataParser.rb
+++ app/models/HTMLVoterDataParser.rb
@@ -2,6 +2,7 @@ class HTMLVoterDataParser
   require 'open-uri'
   require 'nokogiri'
   require 'json'
+  require 'csv'
 
   def parse
     @doc = Nokogiri::HTML(open("https://www.usvotefoundation.org/vote/state-elections/state-voting-laws-requirements.htm"))
@@ -13,12 +14,13 @@ class HTMLVoterDataParser
     55.times do 
       state = Hash.new
       state["name"] = @rows[counter].search("td").first.text.strip
-      state["Early In-Person Voting"] = @rows[counter].search("td:nth-child(2)").text.strip
-      state["No Excuse Absentee Voting"] = @rows[counter].search("td:nth-child(3)").text.strip
-      state["Absentee Voting with Excuse"] = @rows[counter].search("td:nth-child(4)").text.strip
-      state["Same Day Voter Registration"] = @rows[counter].search("td:nth-child(5)").text.strip
-      state["All-Mail Voting"] = @rows[counter].search("td:nth-child(6)").text.strip
-      state["Voter ID on Election Day"] = @rows[counter].search("td:nth-child(7)").text.strip
+      state["Early In-Person Voting"] = (@rows[counter].search("td:nth-child(6)").search('.glyphicon-ok').length > 0 ? "✓" : ' ')
+      state["No Excuse Absentee Voting"] = (@rows[counter].search("td:nth-child(5)").search('.glyphicon-ok').length > 0 ? "✓" : ' ')
+      state["Absentee Voting with Excuse"] = (@rows[counter].search("td:nth-child(4)").search('.glyphicon-ok').length > 0 ? "✓" : ' ')
+      state["Same Day Voter Registration"] = (@rows[counter].search("td:nth-child(3)").search('.glyphicon-ok').length > 0 ? "✓" : ' ')
+      state["All-Mail Voting"] = (@rows[counter].search("td:nth-child(8)").search('.glyphicon-ok').length > 0 ? "✓" : ' ')
+      state["Provisional Voting"] = (@rows[counter].search("td:nth-child(9)").search('.glyphicon-ok').length > 0 ? "✓" : ' ')
+      
       @data << state
       counter += 1
     end
@@ -27,6 +29,48 @@ class HTMLVoterDataParser
   def save_data
     File.open("public/state_data.json","w") do |f|
       f.write(@data.to_json)
+    end
+  end
+
+  def update_data
+    parse
+    save_data
+    StateVotingInformation.all.delete_all
+    state_data = JSON.parse(File.read('public/state_data.json'))
+    state_data.each do |record|
+      if record["Early In-Person Voting"].include?("✓") || record["Early In-Person Voting"].include?("✓ ")
+        record["Early In-Person Voting"] = true
+      else
+        record["Early In-Person Voting"] = false
+      end
+
+      if record["No Excuse Absentee Voting"].include?("✓") || record["No Excuse Absentee Voting"].include?("✓ ")
+        record["No Excuse Absentee Voting"] = true
+      else
+        record["No Excuse Absentee Voting"] = false
+      end
+
+      if record["Absentee Voting with Excuse"].include?("✓") || record["Absentee Voting with Excuse"].include?("✓ ")
+        record["Absentee Voting with Excuse"] = true
+      else
+        record["Absentee Voting with Excuse"] = false
+      end
+
+      if record["Same Day Voter Registration"].include?("✓") || record["Same Day Voter Registration"].include?("✓ ")
+        record["Same Day Voter Registration"] = true
+      else
+        record["Same Day Voter Registration"] = false
+      end
+
+      if record["Provisional Voting"].include?("✓") || record["Provisional Voting"].include?("✓ ")
+        record["Provisional Voting"] = true
+      else
+        record["Provisional Voting"] = false
+      end
+
+      StateVotingInformation.create!(
+          name: record["name"],
+          early_in_person_voting: record["Early In-Person Voting"],no_excuse_absentee_voting: record["No Excuse Absentee Voting"],  absentee_voting_with_excuse: ["Absentee Voting with Excuse"],same_day_voter_registration: record["Same Day Voter Registration"], all_mail_voting: record["All-Mail Voting"], provisional_voting: record["Provisional Voting"])
     end
   end
 
--- lib/tasks/db.rake
+++ lib/tasks/db.rake
@@ -6,4 +6,11 @@ namespace :db do
   	parsed.update_data
   end
 
+  desc "Update state elections by scraping the data and writing it to database"
+  task update_state_voter_info: :environment do
+  	load 'app/models/HTMLVoterDataParser.rb'
+  	parsed = HTMLVoterDataParser.new
+	parsed.update_data
+  end
+
 end
