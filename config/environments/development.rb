2f52902942251220ca5851638d4ff6307db25664
Kyle Dolezal <kyle@piktochart.com>
2017-01-08 23:49:42 +0800

    Use redis for cache storage so caching will be efficient on Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,9 +9,12 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:06dddf60132450709403b8d029b126eb@crestfish.redistogo.com:11605/'
+  config.cache_store = :redis_store
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
