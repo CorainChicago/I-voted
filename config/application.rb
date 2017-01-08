641ceb56cd8519f028ec57131f74bb67c4a8e687
Kyle Dolezal <kyle@piktochart.com>
2017-01-08 20:48:39 +0800

    Fix indentation in config file

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module IVoted
           env_file = File.join(Rails.root, 'config', 'local_env.yml')
           YAML.load(File.open(env_file)).each do |key, value|
             ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
+          end if File.exists?(env_file)
+    end
   end
 end
